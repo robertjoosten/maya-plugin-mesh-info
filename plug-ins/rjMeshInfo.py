f67c91eff97243b8a70715455e724af8b0e2c732
Robert Joosten <rwm.joosten@gmail.com>
2017-10-08 23:56:25 +0100

    documentation

--- plug-ins/rjMeshInfo.py
+++ plug-ins/rjMeshInfo.py
@@ -1,37 +1,60 @@
-"""					
-I N S T A L L A T I O N:
-    Copy the "rjMeshInfo.py" file in any of the directories that are in your 
-    MAYA_PLUG_IN_PATH environment variable:
-        C:\Program Files\Autodesk\<MAYA_VERSION>\plug-ins
-        
-    Copy all the png files in any of the directories that are in your 
-    XBMLANGPATH environment variable:
-        C:\Program Files\Autodesk\<MAYA_VERSION>\icons
+"""			
+This plugin will register a node and a command. With the command it will be 
+possible to query the volume of a mesh and with the node you can query not 
+only the volume but also the surface area of all the faces.
 
-N O T E:
-    This plugin will register a node and a command. With the command it will 
-    be possible to query the volume of a mesh and with the node you can query 
-    not only the volume but also the surface area of all the faces.
+.. figure:: https://github.com/robertjoosten/rjMeshInfo/raw/master/icons/meshInfo.png
+   :align: center
 
-U S A G E:
-    Command:
-        volume = cmds.polyVolume(ws=True, ch=False)
-        worldSpace = True/False
-        constructionHistory = True/False
-            
-        By setting the constructionHistory argument to True, instead of the 
-        volume being returned as a float, the meshInfo node will be returned 
-        that in order can be used to query the volume.
+Installation
+============
+Copy the "rjMeshInfo.py" file in any of the directories that are in your 
+MAYA_PLUG_IN_PATH environment variable   
+::
+    C://Program Files//Autodesk//<MAYA_VERSION>//plug-ins
+    
+Copy all the png files in any of the directories that are in your 
+XBMLANGPATH environment variable
+::
+    C://Program Files//Autodesk//<MAYA_VERSION>//icons
+		
+Note
+====
+This plugin will register a node and a command. With the command it will 
+be possible to query the volume of a mesh and with the node you can query 
+not only the volume but also the surface area of all the faces.
+
+Usage
+=====
+Command
+::
+    volume = cmds.polyVolume(ws=True, ch=False)
+    worldSpace = True/False
+    constructionHistory = True/False
+    
+By setting the constructionHistory argument to True, instead of the 
+volume being returned as a float, the meshInfo node will be returned 
+that in order can be used to query the volume.
+         
+Node
+::
+    meshInfo = cmds.createNode("meshInfo")
+    
+    cmds.connectAttr(
+        "{0}.worldSpace[0]".format(mesh), 
+        "{0}.inMesh".format(meshInfo)
+    )
+    volume = cmds.getAttr("{0}.volume".format(meshInfo))
+    area = cmds.getAttr("{0}.area".format(meshInfo))
         
-    Node:
-        meshInfo = cmds.createNode("meshInfo")
-        
-        cmds.connectAttr(
-            "{0}.worldSpace[0]".format(mesh), 
-            "{0}.inMesh".format(meshInfo)
-        )
-        volume = cmds.getAttr("{0}.volume".format(meshInfo))
-        area = cmds.getAttr("{0}.area".format(meshInfo))
+Note
+====
+This plugin will register a node and a command. With the command it will 
+be possible to query the volume of a mesh and with the node you can query 
+not only the volume but also the surface area of all the faces.
+
+Code
+====
 """
 
 __author__   = "Robert Joosten"
